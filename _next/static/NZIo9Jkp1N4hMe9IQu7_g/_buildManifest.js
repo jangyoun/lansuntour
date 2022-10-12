54017302dc9549085e474b9dc157bb8e5c20a498
jangyoun <jangyounc@gmail.com>
2022-10-13 04:23:28 +0900

    init

--- _next/static/NZIo9Jkp1N4hMe9IQu7_g/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-79a50621b0dacd8d.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/aaa":["static/chunks/pages/aaa-6c32206450b47417.js"],sortedPages:["/","/_app","/_error","/aaa"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
